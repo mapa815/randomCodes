55815585ed71388244f82f19cb41132be34b3cf5
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-21 19:45:56 -0400

    Update group anagrams.java

--- leetcode stuff/group anagrams.java
+++ leetcode stuff/group anagrams.java
@@ -7,3 +7,16 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 typically using all the original letters exactly once.
 */
 
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, LinkedList<String>> map = new HashMap<>();
+        
+        for(String s: strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            map.computeIfAbsent(String.valueOf(c), k -> new LinkedList<String>()).add(s);
+        }
+        
+        return new LinkedList<>(map.values());
+    }
+}
