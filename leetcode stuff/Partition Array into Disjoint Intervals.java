1c4d0b074408bb4606a9cb3c222936ef1ebfbe43
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-22 20:58:26 -0400

    Update Partition Array into Disjoint Intervals.java

--- leetcode stuff/Partition Array into Disjoint Intervals.java
+++ leetcode stuff/Partition Array into Disjoint Intervals.java
@@ -9,3 +9,20 @@ left has the smallest possible size.
 Return the length of left after such a partitioning.  It is guaranteed that such a partitioning exists.
 */
 
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+        int maxLeft = nums[0];
+        int max = nums[0];
+        int p = 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<maxLeft){
+                maxLeft = max;
+                p=i;
+            } else if(max<nums[i]) {
+                max=nums[i];
+            }
+        }
+        
+        return p+1;
+    }
+}
