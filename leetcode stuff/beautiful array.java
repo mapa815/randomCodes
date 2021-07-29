44fc62b329cafb299e987fb2478c309a9b7d8bca
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-28 21:15:33 -0400

    Update beautiful array.java

--- leetcode stuff/beautiful array.java
+++ leetcode stuff/beautiful array.java
@@ -9,14 +9,14 @@ Given n, return any beautiful array nums.  (It is guaranteed that one exists.)
 /*
 ALGORITHM EXPLANATION
 
-Sort the array nums, ans := 0, diff := Infinity, n := size of nums
-for i in range 0 to n – 1
-left := i + 1, right := n – 1
-while left < right
-temp := nums[left] + nums[right] + nums[i]
-If |target – temp| < diff, then ans := temp and diff := |target – temp|
-If temp = target, then return temp,
-Otherwise when temp > target, then decrease right by 1, else increase left by 1
+Create a vector called ans, insert 1 into ans
+While size of ans < N
+create a vector temp
+for i in range 0 to size of ans – 1
+if ans[i] * 2 – 1 <= N, then insert ans[i] * 2 – 1 into temp array
+for i in range 0 to size of ans – 1
+if ans[i] * 2 <= N, then insert ans[i] * 2 into temp array
+set ans := temp
 return ans
 */
 
