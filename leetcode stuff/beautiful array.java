bd6c8247c5e4f442a417afb00fce6d8c2fb637cc
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-28 21:09:21 -0400

    Create beautiful array.java

---
+++ leetcode stuff/beautiful array.java
@@ -0,0 +1,7 @@
+/*
+PROBLEM STATEMENT
+
+For some fixed n, an array nums is beautiful if it is a permutation of the integers 1, 2, ..., n, such that:
+For every i < j, there is no k with i < k < j such that nums[k] * 2 = nums[i] + nums[j].
+Given n, return any beautiful array nums.  (It is guaranteed that one exists.)
+*/
