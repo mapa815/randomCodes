18f3cfdcd7bd3b82deae18c8cf6509717149257a
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-29 20:29:55 -0400

    Update merge two sorted arrays.java

--- leetcode stuff/merge two sorted arrays.java
+++ leetcode stuff/merge two sorted arrays.java
@@ -4,6 +4,16 @@ PROBLEM STATEMENT
 Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
 */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         //preliminary conditions
