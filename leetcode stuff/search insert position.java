d5fa0c15358d76a3f214ddf7b5325c98db9c2155
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-30 22:18:56 -0400

    Update search insert position.java

--- leetcode stuff/search insert position.java
+++ leetcode stuff/search insert position.java
@@ -6,3 +6,22 @@ If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
 */
+class Solution {
+    public int searchInsert(int[] nums, int t) {
+        int s=0;
+        int e=nums.length-1;
+        int m=0;
+        
+        while(s<nums.length && e>=0 && nums[s] <= nums[e]) {
+            m = s+(e-s)/2;
+            if(nums[m]<t) {
+                s=m+1;
+            } else if(nums[m]==t) {
+                return m;
+            } else if(nums[m]>t) {
+                e=m-1;
+            }
+        }
+        return e+1;
+    }
+}
