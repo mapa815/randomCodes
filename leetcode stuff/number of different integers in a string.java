e2a0b58a700adc05859d28dce27dfd7fb7e58328
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-07-31 18:06:34 -0400

    Update number of different integers in a string.java

--- leetcode stuff/number of different integers in a string.java
+++ leetcode stuff/number of different integers in a string.java
@@ -7,3 +7,15 @@ that are separated by at least one space: "123", "34", "8", and "34".
 Return the number of different integers after performing the replacement operations on word.
 Two integers are considered different if their decimal representations without any leading zeros are different.
 */
+class Solution {
+    public int numDifferentIntegers(String word) {
+        String replaced = word.replaceAll("[a-z]", " "); //replace all a-z characters
+        Set<String> nums = new HashSet();
+        String [] words = replaced.split(" "); //split based on space character
+        for (String s : words) {
+            if (s.trim().length() > 0) //if word is not empty add to the set
+                nums.add(s.replaceAll("^0*", "")); //replace all leading zeros - ^matchs the beginning zeros * as many as zeros will be matched
+        }
+        return nums.size();
+    }
+}
