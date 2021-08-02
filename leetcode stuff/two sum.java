aa6ba89ffe9e6c968a067bcb07c00309a015e320
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-02 19:58:29 -0400

    Update two sum.java

--- leetcode stuff/two sum.java
+++ leetcode stuff/two sum.java
@@ -5,3 +5,19 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 */
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+}
