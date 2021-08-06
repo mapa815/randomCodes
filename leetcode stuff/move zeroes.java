c3cf8995c42213a3c330e52067a743ab1d9595a2
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-05 20:10:26 -0400

    Update move zeroes.java

--- leetcode stuff/move zeroes.java
+++ leetcode stuff/move zeroes.java
@@ -1,5 +1,7 @@
 /*
 PROBLEM STATEMENT
 
-
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
 */
+
