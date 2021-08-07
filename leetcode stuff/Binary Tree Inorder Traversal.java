df50e94be7590d27149940519f117c08ac50899f
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-07 09:56:06 -0400

    Update Binary Tree Inorder Traversal.java

--- leetcode stuff/Binary Tree Inorder Traversal.java
+++ leetcode stuff/Binary Tree Inorder Traversal.java
@@ -3,3 +3,22 @@ PROBLEM STATEMENT
 
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
 */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode temp=root;
+        while(temp!=null || !stack.isEmpty()){
+            while(temp!=null){
+                stack.push(temp);
+                temp=temp.left;
+            }
+            if(!stack.isEmpty() ){
+                TreeNode top = stack.pop();
+                ans.add(top.val);
+                temp=top.right;
+            }
+        }
+        return ans;
+    }
+}
