ce07a5224d7041b4811acb7f971effcf230a6b8d
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-13 20:23:43 -0400

    Update word pattern.java

--- leetcode stuff/word pattern.java
+++ leetcode stuff/word pattern.java
@@ -11,3 +11,31 @@ Example 2:
 Input: pattern = "abba", s = "dog cat cat fish"
 Output: false
 */
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        Set<Character> setChar=new HashSet<>();
+        Set<String> setString=new HashSet<>();
+            String[] sSplit = s.split(" ");
+		if (pattern.length() != sSplit.length) {
+			return false;
+		} 
+        
+        else {
+			for (int i = 0; i < pattern.length(); i++) {
+				char c=pattern.charAt(i);
+                setChar.add(c);
+				if (!map.containsKey(c)) {
+					map.put(c, sSplit[i]);
+				} else {
+					if(!map.get(c).equals(sSplit[i])){
+						return false;
+					}
+                        map.put(c,sSplit[i]);
+				}
+			}
+        setString.addAll(map.values());
+    }
+        return setString.size()==setChar.size();
+}
+}
