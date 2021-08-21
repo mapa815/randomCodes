184dcca8aa1190538c23d7151defc28a0d6a7275
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-21 19:02:20 -0400

    Update valid parenthesis.java

--- leetcode stuff/valid parenthesis.java
+++ leetcode stuff/valid parenthesis.java
@@ -16,3 +16,24 @@ Example 3:
 Input: s = "(]"
 Output: false
 */
+class Solution {
+ 
+    boolean valid(char b, char e) {
+        return b == '(' && e == ')' || b == '{' && e == '}' || b == '[' && e == ']';
+    }
+
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (Character c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (valid(stack.peek(), c)) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();  
+    } 
+} 
