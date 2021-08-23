522f6d6308fe95367cb1f4b2e4d2bf2920ef6f0c
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-23 17:42:57 -0400

    Update single number.java

--- leetcode stuff/single number.java
+++ leetcode stuff/single number.java
@@ -3,3 +3,12 @@ PROBLEM STATEMENT
 
 
 */
+class Solution {
+    public int singleNumber(int[] nums) 
+    {         
+        int singleNum = 0;
+        for(int i = 0; i < nums.length; i++)
+            singleNum ^= nums[i];
+        return singleNum;
+    }
+}
