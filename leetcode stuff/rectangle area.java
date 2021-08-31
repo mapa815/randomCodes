49fccd20a8cc7aa6e20ebc3f4a3fefa09e91c318
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-08-30 22:05:09 -0400

    Update rectangle area.java

--- leetcode stuff/rectangle area.java
+++ leetcode stuff/rectangle area.java
@@ -5,6 +5,7 @@ Given the coordinates of two rectilinear rectangles in a 2D plane, return the to
 The first rectangle is defined by its bottom-left corner (ax1, ay1) and its top-right corner (ax2, ay2).
 The second rectangle is defined by its bottom-left corner (bx1, by1) and its top-right corner (bx2, by2).
 
+Example 1:
 Input: ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
 Output: 45
 Example 2:
