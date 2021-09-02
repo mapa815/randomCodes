3c4a7cf07de19ea5bc8a23435f35dcd4c24b3b57
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-01 22:10:53 -0400

    Update median of two sorted arrays.java

--- leetcode stuff/median of two sorted arrays.java
+++ leetcode stuff/median of two sorted arrays.java
@@ -4,8 +4,6 @@ PROBLEM STATEMENT
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
 
- 
-
 Example 1:
 Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
