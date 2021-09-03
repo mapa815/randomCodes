7c68dbf5019c25ddbe234e752aab2e175e32a0e6
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-03 14:25:06 -0400

    Update permutations II.cpp

--- leetcode stuff/permutations II.cpp
+++ leetcode stuff/permutations II.cpp
@@ -1,7 +1,17 @@
 /*
 PROBLEM STATEMENT
 
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 
+Example 1:
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
 class Solution {
 public:
