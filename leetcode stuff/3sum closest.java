ec2bc16dd784cec5ea52fbba25384e30db4346c8
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-04 14:54:32 -0400

    Update 3sum closest.java

--- leetcode stuff/3sum closest.java
+++ leetcode stuff/3sum closest.java
@@ -13,3 +13,31 @@ Example 2:
 Input: nums = [0,0,0], target = 1
 Output: 0
 */
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0] + nums[1] + nums[nums.length-1];
+        Arrays.sort(nums);
+
+        for (int start = 0; start < nums.length - 2; start++) {
+            int middle = start + 1;
+            int end = nums.length - 1;
+            
+            while (middle < end) {
+                int sum = nums[start] + nums[middle] + nums[end];  
+                if (sum == target) {
+                    return target;
+                } else if (sum > target) {
+                     while(middle<end && nums[end] == nums[end-1]) end--;
+                     end--;
+                } else {
+                    while(middle<end && nums[middle] == nums[middle+1]) middle++;
+                    middle++;
+                }
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+}
