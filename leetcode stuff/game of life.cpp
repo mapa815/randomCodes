4e8e43d60587529e81a7bda92209211a6264f884
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-03 21:43:06 -0400

    Update game of life.cpp

--- leetcode stuff/game of life.cpp
+++ leetcode stuff/game of life.cpp
@@ -10,4 +10,11 @@ Any live cell with more than three live neighbors dies, as if by over-population
 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. 
 Given the current state of the m x n grid board, return the next state.
+
+Example 1:
+Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+
+Example 2:
+
 */
