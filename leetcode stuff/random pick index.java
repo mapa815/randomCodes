41db9939dd3aaa41f257a6fb7d858161b9ca2f47
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-05 16:37:39 -0400

    Update random pick index.java

--- leetcode stuff/random pick index.java
+++ leetcode stuff/random pick index.java
@@ -20,3 +20,32 @@ solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each ind
 solution.pick(1); // It should return 0. Since in the array only nums[0] is equal to 1.
 solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
 */
+
+class Solution {
+    Map<Integer, List<Integer>> map = new HashMap();
+    public Solution(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                List<Integer> list = new LinkedList<>();
+                list.add(i);
+                map.put(nums[i],list);
+            }else{
+                List<Integer> list = map.get(nums[i]);
+                list.add(i);
+            }
+        }
+    }
+    
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        Random r = new Random();
+        int b= r.nextInt(list.size());
+        return list.get(b);
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
