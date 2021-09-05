aaac2f0786c5df7bbc50b7145724f311db02ed19
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-05 17:37:56 -0400

    Update valid perfect square.java

--- leetcode stuff/valid perfect square.java
+++ leetcode stuff/valid perfect square.java
@@ -11,3 +11,20 @@ Example 2:
 Input: num = 14
 Output: false
 */
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 1;
+        long end = num;
+        while(start <=  end)
+        {
+            long mid = start + (end - start)/2;
+            if(mid * mid == num)
+                return true;
+            else if(mid * mid > num)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return false;
+    }
+}
