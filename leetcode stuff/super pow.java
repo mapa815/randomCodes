395b328528e110f8e33a99285ab651f87ba9191b
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-05 17:56:31 -0400

    Update super pow.java

--- leetcode stuff/super pow.java
+++ leetcode stuff/super pow.java
@@ -1,4 +1,21 @@
 /*
 PROBLEM STATEMENT
 
+Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+
+Example 1:
+Input: a = 2, b = [3]
+Output: 8
+
+Example 2:
+Input: a = 2, b = [1,0]
+Output: 1024
+
+Example 3:
+Input: a = 1, b = [4,3,3,8,5,2]
+Output: 1
+
+Example 4:
+Input: a = 2147483647, b = [2,0,0]
+Output: 1198
 */
