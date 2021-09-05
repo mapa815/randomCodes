ac6d93056ddb771aa929eed26c8d8f9c4ff1d3b4
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-05 17:28:02 -0400

    Update and rename sum of square numbers.java to sum of square numbers.cpp

--- leetcode stuff/sum of square numbers.cpp
+++ leetcode stuff/sum of square numbers.cpp
@@ -20,3 +20,19 @@ Example 5:
 Input: c = 1
 Output: true
 */
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a=0,b=sqrt(c);
+        while(a<=b)
+        {
+            if((a*a+b*b)==c)
+                return true;
+            else if((a*a+b*b)>c)
+                b--;
+            else
+                a++;
+        }
+        return false;
+    }
+};
