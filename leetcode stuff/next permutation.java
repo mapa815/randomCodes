a8ec63b95febfeee34bb5f60de4bd7e24aa0a6b5
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-06 19:10:23 -0400

    Update next permutation.java

--- leetcode stuff/next permutation.java
+++ leetcode stuff/next permutation.java
@@ -21,3 +21,48 @@ Example 4:
 Input: nums = [1]
 Output: [1]
 */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums.length == 1)
+            return;
+        int i = nums.length -1;
+        while(i > 0 && nums[i-1] >= nums[i]){
+            i--;
+        }
+        
+        reverse(nums, i);
+        if(i == 0) //Next Permutation not possible
+            return;
+        int swapIndex = binarySearch(nums, i , nums.length-1, nums[i-1]); // Pick num greater than nums[i-1]
+        swap(nums, swapIndex, i-1);
+    }
+    
+    public void reverse(int[] nums, int start){
+        int half = start + (nums.length - start)/2;
+        int end = nums.length-1;
+        while(start < half){
+            swap(nums, start , end);
+            start++;
+            end--;
+        }
+    }
+    
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    
+    public int binarySearch(int[] nums, int start , int end, int find){
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] > find){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        
+        return start;
+    }
+}
