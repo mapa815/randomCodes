fbb4559b3faa57efa4800c8d7bd64097bf47782f
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-09 20:03:04 -0400

    Update Maximum Product of Word Lengths.java

--- leetcode stuff/Maximum Product of Word Lengths.java
+++ leetcode stuff/Maximum Product of Word Lengths.java
@@ -19,3 +19,29 @@ Input: words = ["a","aa","aaa","aaaa"]
 Output: 0
 Explanation: No such pair of words.
 */
+class Solution {
+    public int maxProduct(String[] words) {
+        int max = 0; 
+        for (int i = 0; i < words.length - 1; i++) {     // Compare current word
+            String word1 = words[i];
+            for (int j = i + 1; j < words.length; j++) {     // With each remain word
+                String word2 = words[j];
+                boolean failed = false;
+                char prev = ' ';
+                for (char c : word2.toCharArray()) {     // Compare all characters in the pair of words.
+                    if (prev != c) {       // Dont bother checking consecutive repeated characters
+                        if (word1.contains(String.valueOf(c))) {   
+                            failed = true;     
+                            break;      
+                        }
+                        prev = c;
+                    }
+                }
+                if (!failed) {    
+                    max = Math.max(max, word1.length() * word2.length());    
+                }
+            }
+        }
+        return max; 
+     }
+}
