270a161f1415a5d94656fa7e47167504c5c3c18a
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-10 16:44:45 -0400

    Update mini parser.java

--- leetcode stuff/mini parser.java
+++ leetcode stuff/mini parser.java
@@ -21,3 +21,88 @@ Explanation: Return a NestedInteger object containing a nested list with 2 eleme
     ii. A nested list with one element:
          a. An integer containing value 789
 */
+
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public NestedInteger deserialize(String s) {
+        if (!s.contains("[")) { // corner case, just int.
+            return new NestedInteger(Integer.valueOf(s));
+        }
+        
+        NestedInteger res = new NestedInteger();
+        Stack<NestedInteger> stk = new Stack<>();
+        int sign = 1;
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + c - '0';
+            }
+            else if (c == '-') {
+                sign = -1;
+            }
+            else if (c == ',') {
+                if (s.charAt(i - 1) != ']') { // there can be '[' before ,
+                     NestedInteger intNi = new NestedInteger(num * sign);
+                     stk.push(intNi);
+                }
+                sign = 1;
+                num = 0; // only need sign and num update here
+            }
+            else if (c == '[') {
+                NestedInteger ni = null; // smart to push null as '[' mark
+                stk.push(ni);
+            }
+            else { // ]
+                // Only when previous chars are number, then do this
+                if (i > 0 && s.charAt(i - 1) >= '0' && 
+                             s.charAt(i - 1) <= '9') { 
+                    NestedInteger intNi = new NestedInteger(num * sign);
+                    stk.push(intNi);
+                }
+              
+                NestedInteger ni = new NestedInteger();
+                List<NestedInteger> lst = new ArrayList<>();
+                while (stk.peek() != null) {
+                    lst.add(stk.pop());
+                }
+                for (int j = lst.size() - 1; j >= 0; j--) { // reverse order
+                    ni.add(lst.get(j));
+                }
+                stk.pop();
+                stk.push(ni); // don't forget push for next round
+                res = ni;
+            }
+            
+        }
+        return res;
+    }
+}
