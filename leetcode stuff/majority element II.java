dda89bec86e6cf07b17096667a5802b668836b7e
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-10 20:35:46 -0400

    Update majority element II.java

--- leetcode stuff/majority element II.java
+++ leetcode stuff/majority element II.java
@@ -19,3 +19,28 @@ Example 3:
 Input: nums = [1,2]
 Output: [1,2]
 */
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> numArray = new ArrayList<>();
+        
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+        
+        int limit = nums.length/3;
+        for(int key: map.keySet() ){
+            if(map.get(key)>limit){
+                numArray.add(key);
+            }
+        }
+        return numArray;
+    }
+}
