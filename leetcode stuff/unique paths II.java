0bfe5a5b1fe3ca9abbd183111802b731ff021b89
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-11 11:45:30 -0400

    Update unique paths II.java

--- leetcode stuff/unique paths II.java
+++ leetcode stuff/unique paths II.java
@@ -21,3 +21,49 @@ Example 2:
 Input: obstacleGrid = [[0,1],[0,0]]
 Output: 1
 */
+class Solution {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+
+        boolean flag = false;
+
+        // fill 1st column
+        for (int i = 0; i < col; i++) {
+            if (flag || obstacleGrid[0][i] == 1) {
+                flag = true;
+                obstacleGrid[0][i] = 0;
+            } else {
+                obstacleGrid[0][i] = 1;
+            }
+        }
+
+        flag = false;
+
+        // fill 1st row
+        for (int i = 1; i < row; i++) {
+            if (flag || obstacleGrid[i][0] == 1) {
+                flag = true;
+                obstacleGrid[i][0] = 0;
+            } else {
+                obstacleGrid[i][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                } else {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                }
+            }
+        }
+
+        return obstacleGrid[row - 1][col - 1];
+    }
+}
