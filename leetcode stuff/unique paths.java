fc8f60e826945a97632e959e862fac4438f0dd89
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-11 11:44:09 -0400

    Update unique paths.java

--- leetcode stuff/unique paths.java
+++ leetcode stuff/unique paths.java
@@ -29,3 +29,20 @@ Example 4:
 Input: m = 3, n = 3
 Output: 6
 */
+class Solution {
+    public static int uniquePaths(int m, int n) {
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+                }
+            }
+        }
+
+        return matrix[m - 1][n - 1];
+    }
+}
