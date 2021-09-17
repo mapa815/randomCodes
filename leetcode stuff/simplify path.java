b9dde9ce99d0971096bf3a029fb4c652bcb780ec
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-17 18:15:44 -0400

    Update simplify path.java

--- leetcode stuff/simplify path.java
+++ leetcode stuff/simplify path.java
@@ -37,3 +37,16 @@ Example 4:
 Input: path = "/a/./b/../../c/"
 Output: "/c"
 */
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
+        }
+        String res = "";
+        for (String dir : stack) res = "/" + dir + res;
+        return res.isEmpty() ? "/" : res;
+    }
+}
