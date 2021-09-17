abf3d09f8f9fd07aa6b2c0272ca52d482030faf7
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-16 20:14:53 -0400

    Update word search.java

--- leetcode stuff/word search.java
+++ leetcode stuff/word search.java
@@ -17,3 +17,26 @@ Example 3:
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
 Output: false
 */
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+          for (int j = 0; j < board[0].length; j++) {
+            if (dfs(board, word, i, j, 0)) return true;
+          }
+        }
+        return false;
+    }
+
+  private static boolean dfs(char[][] B, String word, int i, int j, int w) {
+    if (w == word.length()) return true;
+    if (i < 0 || i >= B.length || j < 0 || j >= B[0].length || B[i][j] == '#' 
+            || B[i][j] != word.charAt(w)) return false;
+    int[][] SHIFT = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    B[i][j] = '#';
+    for (int[] dir : SHIFT) {
+      if (dfs(B, word, i + dir[0], j + dir[1], w + 1)) return true;
+    }
+    B[i][j] = word.charAt(w);
+    return false;
+  }
+}
