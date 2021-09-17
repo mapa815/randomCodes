3ee1e391139ff9ca44993c8db314ca9135891afc
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-16 20:17:39 -0400

    Update partition list.java

--- leetcode stuff/partition list.java
+++ leetcode stuff/partition list.java
@@ -12,3 +12,36 @@ Example 2:
 Input: head = [2,1], x = 2
 Output: [1,2]
 */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode head2 = new ListNode();
+        var temp = head2;
+        var itt = head;
+        while(itt != null){
+            if(itt.val < x){
+                temp.next = new ListNode(itt.val);
+                temp = temp.next;
+                itt.val = -101;
+            }
+            itt = itt.next;
+        }
+        while(head != null){
+            if(head.val >= x && head.val != -101){
+                temp.next = new ListNode(head.val);
+                temp = temp.next;
+            }
+            head = head.next;
+        }
+        return head2.next;
+    }
+}
