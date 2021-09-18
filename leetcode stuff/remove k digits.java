4f97a6f0eab1f8a6477ded713e018fdb1006e763
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-18 11:36:14 -0400

    Update remove k digits.java

--- leetcode stuff/remove k digits.java
+++ leetcode stuff/remove k digits.java
@@ -18,3 +18,31 @@ Input: num = "10", k = 2
 Output: "0"
 Explanation: Remove all the digits from the number and it is left with nothing which is 0.
 */
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int len = num.length();
+        if(k == 0)  return num;
+        if(k == len) return "0";
+        
+        Stack<Character> stack = new Stack<>();
+        int index = 0;
+        
+        while(index < len){
+            while(k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(index)){
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(index++));
+        }
+        while(k-- > 0) stack.pop();
+        
+        String smallest = "";
+        while(!stack.isEmpty()) smallest = stack.pop() + smallest;
+        
+		// delete leading zeros
+        while(smallest.length() > 1 && smallest.charAt(0) == '0')
+            smallest = smallest.substring(1);
+        
+        return smallest;
+    }
+}
