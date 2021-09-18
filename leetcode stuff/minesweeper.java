94bfc6a620462c956db5ea8735e59771aa312aba
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-09-18 12:38:52 -0400

    Update minesweeper.java

--- leetcode stuff/minesweeper.java
+++ leetcode stuff/minesweeper.java
@@ -25,3 +25,40 @@ Example 2:
 Input: board = [["B","1","E","1","B"],["B","1","M","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]], click = [1,2]
 Output: [["B","1","E","1","B"],["B","1","X","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]]
 */
+public class Solution {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length, n = board[0].length;
+        int row = click[0], col = click[1];
+        
+        if (board[row][col] == 'M') { // Mine
+            board[row][col] = 'X';
+        }
+        else { // Empty
+            // Get number of mines first.
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+            if (count > 0) { // If it is not a 'B', stop further DFS.
+                board[row][col] = (char)(count + '0');
+            }
+            else { // Continue DFS to adjacent cells.
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'E') updateBoard(board, new int[] {r, c});
+                    }
+                }
+            }
+        }
+        return board;
+    }
+}
