8605a8368866ca36a55d9ae37a4e245b7e52f0b8
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-10-02 20:02:31 -0400

    Create h index I.java

---
+++ leetcode stuff/h index I.java
@@ -0,0 +1,41 @@
+/*
+PROBLEM STATEMENT
+
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return compute the researcher's h-index.
+According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, 
+and the other n − h papers have no more than h citations each.
+
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+Example 1:
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Example 2:
+Input: citations = [1,3,1]
+Output: 1
+*/
+class Solution {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int[] count = new int[len + 1];
+
+        for (int c: citations)
+            if (c > len) 
+                count[len]++;
+            else 
+                count[c]++;
+
+
+        int total = 0;
+        for (int i = len; i >= 0; i--) {
+            total += count[i];
+            if (total >= i)
+                return i;
+        }
+
+        return 0;
+    }
+}
