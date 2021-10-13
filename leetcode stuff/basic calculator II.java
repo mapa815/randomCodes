c1b674448a08f2227e549808b27c813929e13034
mapa815 <36806172+mapa815@users.noreply.github.com>
2021-10-12 20:05:41 -0400

    Create basic calculator II.java

---
+++ leetcode stuff/basic calculator II.java
@@ -0,0 +1,54 @@
+/*
+PROBLEM STATEMENT
+
+Given a string s which represents an expression, evaluate this expression and return its value. 
+The integer division should truncate toward zero.
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+Example 1:
+Input: s = "3+2*2"
+Output: 7
+
+Example 2:
+Input: s = " 3/2 "
+Output: 1
+
+Example 3:
+Input: s = " 3+5 / 2 "
+Output: 5
+*/
+class Solution {
+    public int calculate(String s) {
+        int sum = 0;
+        int tempSum = 0;
+        int num = 0;
+        char lastSign = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) num = num * 10 + c - '0';
+            if (i == s.length() - 1 || !Character.isDigit(c) && c!=' ') {
+                switch(lastSign) {
+                    case '+':
+                        sum+=tempSum;
+                        tempSum = num;
+                        break;
+                    case '-':
+                        sum+=tempSum;
+                        tempSum = -num;
+                        break;
+                    case '*':
+                        tempSum *= num;
+                        break;
+                    case '/':
+                        tempSum /= num;
+                        break;
+                }
+                lastSign = c;
+                num=0;
+            }
+        }
+        sum+=tempSum;
+        return sum;
+    }
+}
