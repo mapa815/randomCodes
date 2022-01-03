a34800e603378701cddda57c094fe5073989431c
mapa815 <36806172+mapa815@users.noreply.github.com>
2022-01-03 05:16:47 -0400

    Update rank transform of an array.java

--- leetcode stuff/rank transform of an array.java
+++ leetcode stuff/rank transform of an array.java
@@ -24,3 +24,15 @@ Output: [4]
 Explanation: After sorting, nums is [1,2,2,3,5].
 The index where nums[i] == 5 is 4.
 */
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] A = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(A);
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        for (int x : A)
+          rank.putIfAbsent(x, rank.size() + 1);
+        for (int i = 0; i < arr.length; ++i)
+          A[i] = rank.get(arr[i]);
+        return A;
+    }
+}
